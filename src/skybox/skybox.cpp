862cc8829265d9dffffd5a4e5dccb896b027786d
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2025-06-14 20:28:25 +0100

    fix: skybox UV wrapping seam

--- src/skybox/skybox.cpp
+++ src/skybox/skybox.cpp
@@ -39,7 +39,7 @@ bool Skybox::load(const std::string& filepath) {
     // Texture parameters for HDR
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     // Determine internal format based on channels (default 3)
